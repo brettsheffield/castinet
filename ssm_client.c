8878d65e7aa15f11d23de01ed76779b35aa40757
Brett Sheffield <bacs@gladserv.com>
2017-01-20 19:51:42 +0000

    added SSM code, but needs fixing

--- ssm_client.c
+++ ssm_client.c
@@ -1,37 +1,71 @@
 /* simple test of ipv6 SSM multicast */
 
+#include <net/if.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 char *addr = "ff3e::1";
 char *port = "4242";
 
-int main()
+void print_usage(char *prog, int ret)
+{
+	printf("usage: %s [--src <source address>]\n", prog);
+	_exit(ret);
+}
+
+void getaddrinfo_error(int e)
+{
+	printf("getaddrinfo error: %i\n", e);
+}
+
+int main(int argc, char **argv)
 {
 	int s_in;
 	int l;
+	int e;
 	struct addrinfo *castaddr = NULL;
 	struct addrinfo *localaddr = NULL;
+	struct addrinfo *srcaddr = NULL;
 	struct addrinfo hints = {0};
 	struct ipv6_mreq req;
+	struct group_source_req grp;
 	char buf[1024];
+	char *src = NULL;
+
+	/* check args */
+	if (argc > 1) {
+		if (argc == 3) {
+			if (strcmp(argv[1], "--src") == 0) {
+				src = argv[2];
+			}
+			else {
+				print_usage(argv[0], 1);
+			}
+		}
+		else {
+			print_usage(argv[0], 1);
+		}
+	}
 
 	hints.ai_family = AF_INET6;
 	hints.ai_socktype = SOCK_DGRAM;
 	hints.ai_flags = AI_NUMERICHOST;
-	if (getaddrinfo(addr, port, &hints, &castaddr) != 0) {
+	if ((e = getaddrinfo(addr, port, &hints, &castaddr)) != 0) {
 		perror("getaddrinfo (out)");
+		getaddrinfo_error(e);
 		goto main_fail;
 	}
 
 	/* find local address to bind to */
 	hints.ai_flags = AI_PASSIVE;
-	if (getaddrinfo(NULL, port, &hints, &localaddr) != 0) {
+	if ((e = getaddrinfo(NULL, port, &hints, &localaddr)) != 0) {
 		perror("getaddrinfo (in)");
+		getaddrinfo_error(e);
 		goto main_fail;
 	}
 
@@ -47,18 +81,39 @@ int main()
 		goto main_fail;
 	}
 
-
-	memcpy(&req.ipv6mr_multiaddr,
+	/* join multicast group */
+	if (src) {
+		printf("SSM mode (source: %s)\n", src);
+		if ((e = getaddrinfo(src, port, &hints, &srcaddr)) != 0) {
+			getaddrinfo_error(e);
+			goto main_fail;
+		}
+		memset(&grp, 0, sizeof(grp));
+		/* FIXME: use struct sockaddr */
+		memcpy(&grp.gsr_group, castaddr, sizeof(struct sockaddr_in6));
+		memcpy(&grp.gsr_source, srcaddr, sizeof(struct sockaddr_in6));
+		grp.gsr_interface = 0;
+		if (setsockopt(s_in, IPPROTO_IPV6, MCAST_JOIN_SOURCE_GROUP,
+			&grp, sizeof(grp)) == -1)
+		{
+			perror("multicast join");
+			goto main_fail;
+		}
+		freeaddrinfo(srcaddr);
+	}
+	else {
+		printf("ASM modde\n");
+		memcpy(&req.ipv6mr_multiaddr,
 			&((struct sockaddr_in6*)(castaddr->ai_addr))->sin6_addr,
 			sizeof(req.ipv6mr_multiaddr));
-	req.ipv6mr_interface = 0; /* default interface */
 
-	/* join multicast group */
-	if (setsockopt(s_in, IPPROTO_IPV6, IPV6_ADD_MEMBERSHIP, (char *)&req,
-			sizeof(req)) != 0)
-	{
-		perror("multicast join");
-		goto main_fail;
+		req.ipv6mr_interface = 0; /* default interface */
+		if (setsockopt(s_in, IPPROTO_IPV6, IPV6_ADD_MEMBERSHIP,
+			&req, sizeof(req)) != 0)
+		{
+			perror("multicast join");
+			goto main_fail;
+		}
 	}
 
 	freeaddrinfo(localaddr);
