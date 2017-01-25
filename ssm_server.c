521b038d9b29c244db62163bc47a430546d8fe36
Brett Sheffield <bacs@gladserv.com>
2017-01-25 18:40:11 +0000

    process command line args

--- ssm_server.c
+++ ssm_server.c
@@ -3,6 +3,7 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -10,16 +11,71 @@
 
 char *addr = "ff3e::1";
 char *port = "4242";
+char *msg = NULL;
+char *tmp;
 int ttl = 9;
 int loop = 1;
 
-int main()
+void print_usage(char *prog, int ret)
+{
+	printf("usage: %s [--src source address] message\n", prog);
+	_exit(ret);
+}
+
+void process_arg(int *i, char **argv)
+{
+	if (strcmp(argv[*i], "--help") == 0) {
+		print_usage(argv[0], 0);
+	}
+	else if (strcmp(argv[*i], "--addr") == 0) {
+		addr = argv[++(*i)];
+	}
+	else if (strcmp(argv[*i], "--port") == 0) {
+		port = argv[++(*i)];
+	}
+	else {
+		print_usage(argv[0], 1);
+	}
+}
+
+void process_args(int argc, char **argv)
+{
+	int i;
+
+	if (argc > 1) {
+		for (i = 1; i < argc; ++i) {
+			if (strncmp(argv[i], "--", 2) == 0) {
+				if (i == argc)
+					print_usage(argv[0], 1);
+				process_arg(&i, argv);
+			}
+			else {
+				int j = 0;
+				int l = 0;
+				for (j = i; j < argc; j++) l += strlen(argv[j]) + 1;
+				msg = malloc(l);
+				while (i < argc) {
+					strcat(msg, argv[i++]);
+					strcat(msg, " ");
+				}
+				msg[l-1] = '\0';
+				printf("msg: %s\n", msg);
+			}
+		}
+	}
+	if (msg == NULL) {
+		print_usage(argv[0], 1);
+	}
+}
+
+int main(int argc, char **argv)
 {
 	int s_out;
 	int opt;
 	struct addrinfo *castaddr = NULL;
 	struct addrinfo hints = {0};
-	char msg[] = "Barney was here";
+
+	process_args(argc, argv);
 
 	hints.ai_family = AF_INET6;
 	hints.ai_socktype = SOCK_DGRAM;
@@ -57,10 +113,12 @@ int main()
 	freeaddrinfo(castaddr);
 
 	for (;;) {
-		sendto(s_out, msg, sizeof(msg), 0, castaddr->ai_addr,
+		sendto(s_out, msg, strlen(msg), 0, castaddr->ai_addr,
 				castaddr->ai_addrlen);
 		sleep(1);
 	}
+
+	free(msg);
 
 	return 0;
 
